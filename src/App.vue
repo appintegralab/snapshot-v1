90fa2584eb11c03d049cbc0d2e68c8502bab2321
appintegralab <appintegra.lab@gmail.com>
2022-06-25 20:20:22 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <img alt="" src="./assets/logo.svg" />
+  <home />
+</template>
+
+<script>
+import home from './components/home/home.vue'
+
+export default {
+  components: { home }
+}
+</script>
